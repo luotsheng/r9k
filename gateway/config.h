d8a816c6f1a2a1078f9b9cc3f4f748e66b0371b7
luotiansheng <redgogh@vip.qq.com>
2026-03-10 16:56:02 +0800

    config: update align format

--- gateway/config.h
+++ gateway/config.h
@@ -7,14 +7,14 @@
 
 #include <r9k/size.h>
 
-#define GW_SERVER_PORT        26405     /* server port */
+#define GW_SERVER_PORT 26405    /* server port */
 
-#define MAX_EVT         SIZE_KB( 4)     /* max events */
-#define MAX_CNT         SIZE_KB( 4)     /* max message content */
-#define MAX_RB          SIZE_KB( 8)     /* max read buffer */
-#define MAX_WB          SIZE_KB(32)     /* max write buffer */
-#define MAX_TLV         MAX_WB          /* max TLV body */
+#define MAX_EVT SIZE_KB(4)      /* max events */
+#define MAX_CNT SIZE_KB(4)      /* max message content */
+#define MAX_RB SIZE_KB(16)      /* max read buffer */
+#define MAX_WB SIZE_KB(64)      /* max write buffer */
+#define MAX_TLV MAX_WB          /* max TLV body */
 
-#define MAX_IDL                 15      /* max idle timeout sec */
+#define MAX_IDL 15              /* max idle timeout sec */
 
 #endif /* CONFIG_H_ */
